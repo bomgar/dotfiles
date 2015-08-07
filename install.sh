39f0ee35ddc4054a1301d91bb6a6e2b77a79d09e
Patrick Haun <patrick.haun@immobilienscout24.de>
2015-08-07 09:07:50 +0200

    add tmux copy cat plugin

--- install.sh
+++ install.sh
@@ -27,6 +27,11 @@ fi
 ################################################
 echo "Installing tmux config"
 ln -sf $DIR/tmux.conf ~/.tmux.conf
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+  echo "TPM already installed"
+fi
 
 ################################################
 # zsh
