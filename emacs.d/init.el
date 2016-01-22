ad6461c4ca71d1bb07dcd20313092b16b91ac678
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-01-22 22:34:03 +0100

    set whitespace chars

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -43,3 +43,11 @@
 ;move between buffers
 (define-key evil-normal-state-map (kbd "] b") 'next-buffer)
 (define-key evil-normal-state-map (kbd "[ b") 'previous-buffer)
+
+;whitespace style
+(setq whitespace-display-mappings
+  '(
+    (space-mark 32 [183] [46]) ; 32 SPACE 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+    (newline-mark 10 [182 10]) ; 10 LINE FEED
+    (tab-mark 9 [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
+    ))
