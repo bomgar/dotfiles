bbe111767b1d6f9b0c0d6a0bc2e5d711065ba752
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-01-22 21:48:28 +0100

    add some config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq user-full-name "Patrick Haun")
 (setq user-mail-address "bomgar85@googlemail.com")
 
@@ -11,3 +18,36 @@
 (require 'init-evil)
 (require 'init-theme)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (linum+ evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Set to auto read when a file is changed from the outside
+(global-auto-revert-mode t)
+
+;; splits movement
+(global-set-key (kbd "C-h") 'windmove-left)
+(global-set-key (kbd "C-l") 'windmove-right)
+(global-set-key (kbd "C-k") 'windmove-up)
+(global-set-key (kbd "C-j") 'windmove-down)
+
+;; save history
+(setq savehist-additional-variables    ;; also save...
+  '(search-ring regexp-search-ring)    ;; ... my search entries
+  savehist-file "~/.emacs.d/savehist") ;; keep my home clean
+(savehist-mode t)                      ;; do customization before activate
+
+; Set 7 lines to the cursor - when moving vertically
+(setq scroll-margin 7)
+
+; display line numbers
+(global-linum-mode 1)
