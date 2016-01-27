c7ea145a9c3626062a79320669da57d34c5d07a0
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-01-27 16:08:11 +0100

    add indenting

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -110,3 +110,33 @@
 
 ;always follow symlinks
 (setq vc-follow-symlinks t)
+
+;; indent on ret
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+; 2 line indent
+(setq-default tab-width 2 indent-tabs-mode nil)
+
+;; esc quits
+(defun minibuffer-keyboard-quit ()
+      "Abort recursive edit.
+In Delete Selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+      (interactive)
+      (if (and delete-selection-mode transient-mark-mode mark-active)
+          (setq deactivate-mark  t)
+        (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+        (abort-recursive-edit)))
+
+
+
+
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
+(global-set-key [escape] 'evil-exit-emacs-state)
