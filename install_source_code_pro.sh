5cc501b05d1d21fd3003b72f054ac855493307d2
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-03-01 20:20:59 +0100

    install new version

--- install_source_code_pro.sh
+++ install_source_code_pro.sh
@@ -3,9 +3,9 @@ echo "-checking SourceCodePro installation"
 find ~/.fonts -name "*SourceCodePro*" | grep -q '.' || {
     echo "--SourceCodePro not installed, installing"
     FONT_NAME="SourceCodePro"
-    URL="https://github.com/adobe-fonts/source-code-pro/archive/1.017R.zip"
+    URL="https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.zip"
 
-    mkdir  /tmp/adobefont
+    mkdir /tmp/adobefont
     cd /tmp/adobefont
     wget ${URL} -O ${FONT_NAME}.zip
     unzip -o -j ${FONT_NAME}.zip
