c52312fd9cdaa24dd7802ce633a2fe5436a7e0b6
Patrick Haun <bomgar85@googlemail.com>
2016-11-18 08:15:25 +0100

    new sourcecode pro version

--- install_source_code_pro.sh
+++ install_source_code_pro.sh
@@ -3,7 +3,7 @@ echo "-checking SourceCodePro installation"
 find ~/.fonts -name "*SourceCodePro*" | grep -q '.' || {
     echo "--SourceCodePro not installed, installing"
     FONT_NAME="SourceCodePro"
-    URL="https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.zip"
+    URL="https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip"
 
     mkdir /tmp/adobefont
     cd /tmp/adobefont
