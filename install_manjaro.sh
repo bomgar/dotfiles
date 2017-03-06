745467e248acb2e0bc554d1bc76a6608d893379b
Patrick Haun <bomgar85@googlemail.com>
2017-03-06 17:25:59 +0100

    fix script

--- install_manjaro.sh
+++ install_manjaro.sh
@@ -2,4 +2,4 @@
 
 sudo pacman -S sysstat emacs vim tmux git i3-wm zsh feh xautolock scrot imagemagick tig arandr emacs
 
-sudo yaourt -S i3blocks the_silver_searcher
+yaourt -S i3blocks the_silver_searcher
