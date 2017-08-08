5f603df6c86cff9dd1591b05ade3342183beeb23
Patrick Haun <bomgar85@googlemail.com>
2017-08-08 09:41:04 +0200

    [install] openjfx

--- install_manjaro.sh
+++ install_manjaro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S sysstat emacs vim tmux git i3-wm zsh feh xautolock scrot imagemagick tig arandr emacs i3lock gnome-terminal
+sudo pacman -S sysstat emacs vim tmux git i3-wm zsh feh xautolock scrot imagemagick tig arandr i3lock gnome-terminal java-openjfx
 
 yaourt -S i3blocks the_silver_searcher
