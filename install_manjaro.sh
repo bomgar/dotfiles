11c4c249adc49757f8e2cde372d8febef16b6686
Patrick Haun <bomgar85@googlemail.com>
2017-09-06 18:19:36 +0200

    [feat] add dunst notification deamon
    @mriehl this solves the firefox freezing problem for me
    
    also keybase seems to run stable now

--- install_manjaro.sh
+++ install_manjaro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S sysstat emacs vim tmux git i3-wm zsh feh xautolock scrot imagemagick tig arandr i3lock gnome-terminal java-openjfx
+sudo pacman -S sysstat emacs vim tmux git i3-wm zsh feh xautolock scrot imagemagick tig arandr i3lock gnome-terminal java-openjfx dunst
 
 yaourt -S i3blocks the_silver_searcher
