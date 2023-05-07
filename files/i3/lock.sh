73dd63c47dadd5d94fdb58796dc35f0326cc223a
Patrick Haun <patrick@bomgar.email>
2023-05-07 08:36:08 +0200

    use dark theme

--- files/i3/lock.sh
+++ files/i3/lock.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -e
-#SCREENSHOT=/tmp/screen_locked.png
-#SCREENSHOT_BLURED=/tmp/screen_locked_blur.png
-#
-## Take a screenshot
-#scrot $SCREENSHOT
-#
-## blur it
-#convert -blur 0x4 $SCREENSHOT $SCREENSHOT_BLURED
-#rm $SCREENSHOT
+SCREENSHOT=/tmp/screen_locked.png
+SCREENSHOT_BLURED=/tmp/screen_locked_blur.png
+
+# Take a screenshot
+scrot $SCREENSHOT
+
+# blur it
+convert -blur 0x4 $SCREENSHOT $SCREENSHOT_BLURED
+rm $SCREENSHOT
 
 # Lock screen displaying this image.
-i3lock -f -t -i /home/phaun/Downloads/symbiolab.png
+i3lock -f -t -i $SCREENSHOT_BLURED
 
