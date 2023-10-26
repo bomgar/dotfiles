a0f725d9ffc5f3134d9b8dea917fed4d5f0786c2
Patrick Haun <patrick@bomgar.email>
2023-10-26 22:44:03 +0200

    use wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,40 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font = wezterm.font_with_fallback {
+  'Hack Nerd Font Mono',
+  'Noto Sans Mono',
+}
+
+
+-- For example, changing the color scheme:
+local one_dark = wezterm.color.get_builtin_schemes()['OneDark (base16)']
+one_dark.background = "#1E2127"
+
+config.color_schemes = {
+  ['OneDark'] = one_dark,
+}
+
+config.color_scheme = 'OneDark'
+
+config.enable_tab_bar = false
+
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 2,
+  bottom = 2,
+}
+
+
+-- and finally, return the configuration to wezterm
+return config
