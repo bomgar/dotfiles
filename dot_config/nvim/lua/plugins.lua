8a24d3036f053f9f1222e509f3814f321dfeca45
Patrick Haun <patrick@bomgar.email>
2023-10-28 08:08:19 +0200

    completion

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -3,17 +3,16 @@ local plugin_specs = {
   -- auto-completion engine
   {
     "hrsh7th/nvim-cmp",
-    -- event = 'InsertEnter',
     event = "VeryLazy",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "onsails/lspkind-nvim",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-omni",
       "hrsh7th/cmp-emoji",
-      "quangnguyen30192/cmp-nvim-ultisnips",
     },
+    config = function()
+      require("config.nvim-cmp")
+    end,
   },
 
 
