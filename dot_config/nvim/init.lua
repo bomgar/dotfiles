125bdb7e7b60b33eed44c20b31843b210553c346
Patrick Haun <patrick@bomgar.email>
2023-10-28 19:06:54 +0200

    simplify code

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,25 +2,14 @@ vim.loader.enable()
 
 local version = vim.version
 
-
-local core_conf_files = {
-  "globals.lua", -- some global settings
-  "init-lazy.lua", -- some global settings
-  "options.vim", -- setting options in nvim
-  "plugins.lua", -- some global settings
-  "theme.lua",
-}
-
-local viml_conf_dir = vim.fn.stdpath("config") .. "/viml_conf"
--- source all the core config files
-for _, file_name in ipairs(core_conf_files) do
-  if vim.endswith(file_name, 'vim') then
-    local path = string.format("%s/%s", viml_conf_dir, file_name)
-    local source_cmd = "source " .. path
-    vim.cmd(source_cmd)
-  else
-    local module_name, _ = string.gsub(file_name, "%.lua", "")
-    package.loaded[module_name] = nil
-    require(module_name)
-  end
+function source_vim_config(file_name)
+  vim.cmd("source " .. vim.fn.stdpath("config") .. "/viml_conf/" .. file_name)
 end
+
+
+require("globals")
+require("init-lazy")
+source_vim_config("options.vim")
+require("plugins")
+require("theme")
+
