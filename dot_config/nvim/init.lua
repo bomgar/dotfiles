8e7ede8c59b025a17ce84b63971247a1221edca2
Patrick Haun <patrick@bomgar.email>
2023-10-29 21:13:46 +0100

    remove vim source function

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,11 +2,6 @@ vim.loader.enable()
 
 local version = vim.version
 
-function source_vim_config(file_name)
-  vim.cmd("source " .. vim.fn.stdpath("config") .. "/viml_conf/" .. file_name)
-end
-
-
 require("settings")
 require("init-lazy")
 require("plugins")
