3d979ef194db42ba92684acbc35accde1f510f2d
Patrick Haun <patrick@bomgar.email>
2023-11-02 22:21:31 +0100

    use space as leader

--- dot_config/nvim/lua/settings.lua
+++ dot_config/nvim/lua/settings.lua
@@ -1,6 +1,6 @@
 
 -- Custom mapping <leader> (see `:h mapleader` for more info)
-vim.g.mapleader = ','
+vim.g.mapleader = ' '
 
 -- Enable highlighting for lua HERE doc inside vim script
 vim.g.vimsyn_embed = 'l'
