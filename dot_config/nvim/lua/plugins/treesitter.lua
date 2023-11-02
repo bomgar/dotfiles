a2d762011a2244b1ede790a6f6658a5ceb0fe711
Patrick Haun <patrick@bomgar.email>
2023-11-02 08:34:23 +0100

    no treesitter in vscode

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return{
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    enabled = not vim.g.vscode,
     config = function ()
       local configs = require("nvim-treesitter.configs")
 
