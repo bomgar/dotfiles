12ebddb62ecaa30b5383aab1ef11059ff686a49c
Patrick Haun <patrick@bomgar.email>
2023-11-02 15:28:02 +0100

    maintained version

--- dot_config/nvim/lua/plugins/nvim-colorizer.lua
+++ dot_config/nvim/lua/plugins/nvim-colorizer.lua
@@ -3,7 +3,7 @@
 return {
   -- The missing auto-completion for cmdline!
   {
-    "norcalli/nvim-colorizer.lua",
+    "NvChad/nvim-colorizer.lua",
     config = function()
       require('colorizer').setup()
     end
