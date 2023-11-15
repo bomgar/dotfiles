786891950ab0ca67332409f37546e5b14247becc
Patrick Haun <patrick@bomgar.email>
2023-11-15 22:15:20 +0100

    unnecessary semicolon

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -14,19 +14,19 @@ return {
         e = {"<cmd>NvimTreeToggle<CR>", "Toggle NvimTree"},
         f = {
           name = "+find",
-          f = {"<cmd>:Telescope find_files<CR>", "Find File"},
-          g = {"<cmd>:Telescope live_grep<CR>", "Live Grep"},
-          b = {"<cmd>:Telescope buffers<CR>", "Buffers"},
-          h = {"<cmd>:Telescope help_tags<CR>", "Help Tags"}
+          f = {"<cmd>Telescope find_files<CR>", "Find File"},
+          g = {"<cmd>Telescope live_grep<CR>", "Live Grep"},
+          b = {"<cmd>Telescope buffers<CR>", "Buffers"},
+          h = {"<cmd>Telescope help_tags<CR>", "Help Tags"}
         },
         t = {
           name = "+test",
-          r = {"<cmd>:lua require(\"neotest\").run.run()<CR>", "Run test"},
-          d = {"<cmd>:lua require(\"neotest\").run.run({strategy = \"dap\"})<CR>", "Debug test"},
-          f = {"<cmd>:lua require(\"neotest\").run.run(vim.fn.expand(\"%\"))<CR>", "Run current file"},
-          p = {"<cmd>:Neotest output-panel<CR>", "Output panel"},
-          o = {"<cmd>:Neotest output<CR>", "Output"},
-          s = {"<cmd>:Neotest summary<CR>", "summary"},
+          r = {"<cmd>lua require(\"neotest\").run.run()<CR>", "Run test"},
+          d = {"<cmd>lua require(\"neotest\").run.run({strategy = \"dap\"})<CR>", "Debug test"},
+          f = {"<cmd>lua require(\"neotest\").run.run(vim.fn.expand(\"%\"))<CR>", "Run current file"},
+          p = {"<cmd>Neotest output-panel<CR>", "Output panel"},
+          o = {"<cmd>Neotest output<CR>", "Output"},
+          s = {"<cmd>Neotest summary<CR>", "summary"},
         }
       }, { prefix = "<leader>" })
     end
