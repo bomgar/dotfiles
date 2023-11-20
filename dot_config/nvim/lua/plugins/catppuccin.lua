b86111ea41e0d5333f9f54fa500eba9266e637f4
Patrick Haun <patrick@bomgar.email>
2023-11-20 23:45:39 +0100

    better integrations

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -8,6 +8,22 @@ return {
     config = function()
       require("catppuccin").setup({
         flavour = "mocha",
+        integrations = {
+          cmp = true,
+          flash = true,
+          gitsigns = true,
+          harpoon = true,
+          lsp_trouble = true,
+          markdown = true,
+          mason = true,
+          neotest = true,
+          neotree = true,
+          nvimtree = true,
+          rainbow_delimiters = true,
+          telescope = { enabled = true, },
+          treesitter = true,
+          which_key = true,
+        },
       })
       vim.cmd.colorscheme "catppuccin"
     end,
--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
     event = "VeryLazy",
     config = function()
       require('lualine').setup({
-        theme = 'auto'
+        theme = 'catppuccin'
       })
     end,
   },
