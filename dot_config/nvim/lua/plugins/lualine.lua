fca826115c74b8b1c0da6a51ff4e3f462902c550
Patrick Haun <patrick@bomgar.email>
2023-11-20 15:14:05 +0100

    useless code

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -5,7 +5,6 @@ return {
     "nvim-lualine/lualine.nvim",
     enabled = not vim.g.vscode,
     event = "VeryLazy",
-    cond = firenvim_not_active,
     config = function()
       require('lualine').setup({
         theme = 'onedark'
