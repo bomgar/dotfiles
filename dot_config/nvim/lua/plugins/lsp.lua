5eefbf4cb472662a1e2796cd0208f8850d57e70b
Patrick Haun <patrick@bomgar.email>
2023-11-21 20:09:03 +0100

    move setup

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -160,22 +160,25 @@ return {
                 },
               }
             })
-          end
+          end,
+          rust_analyzer = function ()
+
+            local rt = require("rust-tools")
+
+            rt.setup({
+              server = {
+                on_attach = function(_, bufnr)
+                  -- Hover actions
+                  vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+                  -- Code action groups
+                  vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+                end,
+              },
+            })
+          end,
         }
       })
 
-      local rt = require("rust-tools")
-
-      rt.setup({
-        server = {
-          on_attach = function(_, bufnr)
-            -- Hover actions
-            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-            -- Code action groups
-            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-          end,
-        },
-      })
     end
   }
 }
