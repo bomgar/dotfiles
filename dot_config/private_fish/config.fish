c3f97362fe98201dd7499515ee8417262619ec78
Patrick Haun <patrick@bomgar.email>
2023-11-27 12:30:56 +0100

    skim

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -28,7 +28,9 @@ if status is-interactive
   end
 
   if test -x (command -v fw)
-    if test -x (command -v fzf)
+    if test -x (command -v sk)
+      fw print-fish-setup -s | source
+    else if test -x (command -v fzf)
       fw print-fish-setup -f | source
     else
       fw print-fish-setup | source
@@ -65,6 +67,6 @@ if status is-interactive
   set -x PATH ~/bin ~/.cargo/bin ~/go/bin $PATH
   set -x GOPATH ~/go
   set -x FZF_DEFAULT_OPTS "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
-
+  set -x SKIM_DEFAULT_OPTIONS "--color=fg:#cdd6f4,bg:#1e1e2e,matched:#313244,matched_bg:#f2cdcd,current:#cdd6f4,current_bg:#45475a,current_match:#1e1e2e,current_match_bg:#f5e0dc,spinner:#a6e3a1,info:#cba6f7,prompt:#89b4fa,cursor:#f38ba8,selected:#eba0ac,header:#94e2d5,border:#6c7086"
 
 end
