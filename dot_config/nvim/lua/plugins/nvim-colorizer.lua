01122b5c146ff9b3c75ea3c07cccaede5532fded
Patrick Haun <patrick@bomgar.email>
2023-12-04 13:02:53 +0100

    configure colorizer

--- dot_config/nvim/lua/plugins/nvim-colorizer.lua
+++ dot_config/nvim/lua/plugins/nvim-colorizer.lua
@@ -5,7 +5,18 @@ return {
     "NvChad/nvim-colorizer.lua",
     enabled = not vim.g.vscode,
     config = function()
-      require('colorizer').setup()
+      require('colorizer').setup({
+        user_default_options = {
+          names=false,
+        },
+        filetypes = {
+          '*'; -- Highlight all files, but customize some others.
+          css = {css = true, names=true},
+          scss = {css = true, names=true},
+          svelte = {css = true, names=true},
+          astro = {css = true, names=true},
+        },
+      })
     end
   },
 }
