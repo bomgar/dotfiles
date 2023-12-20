36c5d3cb7c38fd4f9cfa0c2909d60491f489002a
Patrick Haun <patrick@bomgar.email>
2023-12-20 18:34:00 +0100

    add copilot

---
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+vim.keymap.set('i', '<C-f>', 'copilot#Accept("<CR>")', {
+    expr = true,
+    replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = 1
+vim.g.copilot_assume_mapped = 1
+
+return {
+    {
+        "github/copilot.vim",
+        enabled = not vim.g.vscode,
+    }
+}
