e23102b97be2866758348a6563edb2ac4f4d511d
Patrick Haun <patrick@bomgar.email>
2023-12-20 19:58:34 +0100

    use lua addon

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,13 +1,24 @@
-vim.keymap.set('i', '<C-f>', 'copilot#Accept("")', {
-    expr = true,
-    replace_keycodes = false
-})
-vim.g.copilot_no_tab_map = 1
-vim.g.copilot_assume_mapped = 1
-
 return {
     {
-        "github/copilot.vim",
-        enabled = not vim.g.vscode,
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        build = ":Copilot auth",
+        event = "InsertEnter",
+        opts = {
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                debounce = 75,
+                keymap = {
+                    accept = "<C-f>",
+                    accept_word = false,
+                    accept_line = false,
+                    next = "<M-]>",
+                    prev = "<M-[>",
+                    dismiss = "<C-]>",
+                },
+            },
+            panel = { enabled = false },
+        },
     }
 }
