4e27170cad37278f92e4b8fb69015f5e7d90c904
Patrick Haun <patrick@bomgar.email>
2024-02-11 17:22:16 +0100

    dependabot

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -118,7 +118,7 @@ return {
                                         ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
                                         ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
                                         ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-                                        ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                                        ["https://json.schemastore.org/dependabot-2.0.json"] = ".github/dependabot.{yml,yaml}",
                                         ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
                                         ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
                                         ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
