43816fd53da6f0632f4f16a14b37ea2fc7b17d98
Patrick Haun <patrick@bomgar.email>
2024-10-20 22:50:03 +0200

    change to meandering markdown plugin

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,4 +1,10 @@
 return {
-
-    { "preservim/vim-markdown", ft = { "markdown" } },
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ft = { "markdown" },
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    }
 }
