c88d76315d895db16a3b87b5a964a8043813981d
Patrick Haun <patrick@bomgar.email>
2024-10-21 09:01:28 +0200

    add grugfar for search and replace

---
+++ dot_config/nvim/lua/plugins/grug-far.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        'MagicDuck/grug-far.nvim',
+        cmd = { "GrugFar" },
+        opts = {
+        },
+        keys = {
+
+            { "<leader>ur", "<cmd>GrugFar<cr>", desc = "search and replace", noremap = true, silent = true },
+        },
+    },
+}
