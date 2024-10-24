404e3cf4571150fcaaadb24644fbef850b1b6eee
Patrick Haun <patrick@bomgar.email>
2024-10-24 17:42:51 +0200

    use config file

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -69,7 +69,6 @@ if status is-interactive
   set -x SUDO_EDITOR nvim
   set -x EDITOR nvim
   set -x PAGER bat
-  set -x NEOVIDE_FORK 1
   set -x WORDCHARS '*?_-.[]~=&;!#$%^(){}<>'
   set -x GOPATH ~/go
   set -x FZF_DEFAULT_OPTS "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
