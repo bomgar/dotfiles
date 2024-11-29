0049a1cf6adaf62ed6cff537f481e7164a5822b4
Patrick Haun <patrick@bomgar.email>
2024-11-29 20:29:56 +0100

    use splitjoin

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -8,12 +8,13 @@ return {
             require("mini.comment").setup({})
             require("mini.trailspace").setup({})
             require("mini.files").setup({})
+            require("mini.splitjoin").setup({})
             local ai = require("mini.ai")
             ai.setup({
                 n_lines = 500,
                 custom_textobjects = {
                     o = ai.gen_spec.treesitter({ -- code block
-                        a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                        a = {"@block.outer", "@conditional.outer", "@loop.outer"},
                         i = { "@block.inner", "@conditional.inner", "@loop.inner" },
                     }),
                     f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
