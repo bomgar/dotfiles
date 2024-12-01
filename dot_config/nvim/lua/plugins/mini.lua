27d6f06e59d0fd5c641ac126abe02837f9c63c7f
Patrick Haun <patrick@bomgar.email>
2024-12-01 19:57:49 +0100

    this is a nvim builtin now

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -5,7 +5,6 @@ return {
         config = function()
             require("mini.icons").setup({})
             require("mini.indentscope").setup({})
-            require("mini.comment").setup({})
             require("mini.trailspace").setup({})
             require("mini.files").setup({})
             require("mini.splitjoin").setup({})
