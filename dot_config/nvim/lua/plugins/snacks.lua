65211241dc028ebc656248585b09ff0e319b0048
Patrick Haun <patrick@bomgar.email>
2024-12-01 16:54:00 +0100

    setup mini integration

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -15,26 +15,23 @@ return {
             { "<c-_>",      function() Snacks.terminal() end,         desc = "which_key_ignore" },
             { "<leader>uc", function() Snacks.bufdelete.other() end,  desc = "close all other buffers" },
         },
-        opts =
-        ---@class snacks.Config
-        ---@field bigfile? snacks.bigfile.Config | { enabled: boolean }
-        ---@field gitbrowse? snacks.gitbrowse.Config
-        ---@field lazygit? snacks.lazygit.Config
-        ---@field notifier? snacks.notifier.Config | { enabled: boolean }
-        ---@field quickfile? { enabled: boolean }
-        ---@field statuscolumn? snacks.statuscolumn.Config  | { enabled: boolean }
-        ---@field terminal? snacks.terminal.Config
-        ---@field toggle? snacks.toggle.Config
-        ---@field styles? table<string, snacks.win.Config>
-        ---@field win? snacks.win.Config
-        ---@field words? snacks.words.Config
-        {
-            styles = {},
-            bigfile = { enabled = true },
-            notifier = { enabled = true },
-            quickfile = { enabled = true },
-            statuscolumn = { enabled = true },
-            words = { enabled = false },
-        }
+        config = function()
+            require("snacks").setup(
+                {
+                    styles = {},
+                    bigfile = { enabled = true },
+                    notifier = { enabled = true },
+                    quickfile = { enabled = true },
+                    statuscolumn = { enabled = true },
+                    words = { enabled = false },
+                }
+            )
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "MiniFilesActionRename",
+                callback = function(event)
+                    Snacks.rename.on_rename_file(event.data.from, event.data.to)
+                end,
+            })
+        end
     }
 }
