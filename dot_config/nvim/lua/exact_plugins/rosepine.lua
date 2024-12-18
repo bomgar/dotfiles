e3357ec226294e3068b45a2d1899e7d04b35ae40
Patrick Haun <patrick@bomgar.email>
2024-12-18 23:12:37 +0100

    colorscheme need high priority

--- dot_config/nvim/lua/exact_plugins/rosepine.lua
+++ dot_config/nvim/lua/exact_plugins/rosepine.lua
@@ -3,9 +3,11 @@ return {
     {
         "rose-pine/neovim",
         name = "rose-pine",
+        lazy = false,     -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000,  -- make sure to load this before all the other start plugins
         config = function()
             require("rose-pine").setup({
-                    variant = "dawn",
+                variant = "dawn",
             })
             vim.cmd("colorscheme rose-pine")
         end
