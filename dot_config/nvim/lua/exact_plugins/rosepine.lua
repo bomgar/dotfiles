a572ebb766bf917b7d2fcedbc34476c09b2e2e3d
Patrick Haun <patrick@bomgar.email>
2024-12-18 14:17:41 +0100

    always use dawn

--- dot_config/nvim/lua/exact_plugins/rosepine.lua
+++ dot_config/nvim/lua/exact_plugins/rosepine.lua
@@ -4,6 +4,9 @@ return {
         "rose-pine/neovim",
         name = "rose-pine",
         config = function()
+            require("rose-pine").setup({
+                    variant = "dawn",
+            })
             vim.cmd("colorscheme rose-pine")
         end
     },
