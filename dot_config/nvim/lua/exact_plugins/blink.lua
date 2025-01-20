09228ab9b615fc861592019ee990fc1d2136df48
Patrick Haun <patrick@bomgar.email>
2025-01-20 16:44:44 +0100

    add documentation border

--- dot_config/nvim/lua/exact_plugins/blink.lua
+++ dot_config/nvim/lua/exact_plugins/blink.lua
@@ -26,10 +26,14 @@ return {
                 use_nvim_cmp_as_default = false,
                 nerd_font_variant = 'mono'
             },
+
             completion = {
                 documentation = {
                     auto_show = true,
                     auto_show_delay_ms = 200,
+                    window = {
+                        border = "double"
+                    }
                 },
             },
 
