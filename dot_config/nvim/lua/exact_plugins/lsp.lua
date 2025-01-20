c0f12d67349317053d5a9d3f6fcd52b2e0bad70f
Patrick Haun <patrick@bomgar.email>
2025-01-20 22:06:54 +0100

    configure json and yaml schemastore

--- dot_config/nvim/lua/exact_plugins/lsp.lua
+++ dot_config/nvim/lua/exact_plugins/lsp.lua
@@ -1,5 +1,10 @@
 return {
 	{
+		"b0o/SchemaStore.nvim",
+		lazy = true,
+		version = false, -- last release is way too old
+	},
+	{
 		"folke/lazydev.nvim",
 		ft = "lua", -- only load on lua files
 		opts = {
@@ -65,18 +70,19 @@ return {
 				automatic_installation = false,
 				ensure_installed = {
 					"astro",
+					"bashls",
 					"cssls",
 					"cssmodules_ls",
 					"gopls",
 					"html",
+					"jsonls",
 					"lua_ls",
+					"pyright",
 					"rust_analyzer",
 					"svelte",
+					"templ",
 					"vtsls",
 					"yamlls",
-					"bashls",
-					"templ",
-					"pyright",
 					"zls"
 				},
 				handlers = {
@@ -130,30 +136,49 @@ return {
 							capabilities = lsp_capabilities,
 						})
 					end,
+					jsonls = function()
+						require("lspconfig").jsonls.setup({
+							capabilities = lsp_capabilities,
+							-- lazy-load schemastore when needed
+							on_new_config = function(new_config)
+								new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+								vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+							end,
+							settings = {
+								json = {
+									format = {
+										enable = true,
+									},
+									validate = { enable = true },
+								},
+							},
+						})
+					end,
 					yamlls = function()
 						require("lspconfig").yamlls.setup({
 							capabilities = lsp_capabilities,
+							-- lazy-load schemastore when needed
+							on_new_config = function(new_config)
+								new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+									"force",
+									new_config.settings.yaml.schemas or {},
+									require("schemastore").yaml.schemas()
+								)
+							end,
 							settings = {
+								redhat = { telemetry = { enabled = false } },
 								yaml = {
-									schemas = {
-										["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-										["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-										["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-										["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-										["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-										["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-										["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-										["https://json.schemastore.org/dependabot-2.0.json"] =
-										".github/dependabot.{yml,yaml}",
-										["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] =
-										"*gitlab-ci*.{yml,yaml}",
-										["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] =
-										"*api*.{yml,yaml}",
-										["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
-										"*docker-compose*.{yml,yaml}",
-										["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] =
-										"*flow*.{yml,yaml}",
-										--kubernetes = "*.yaml",
+									keyOrdering = false,
+									format = {
+										enable = true,
+									},
+									validate = true,
+									schemaStore = {
+										-- Must disable built-in schemaStore support to use
+										-- schemas from SchemaStore.nvim plugin
+										enable = false,
+										-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+										url = "",
 									},
 								},
 							},
