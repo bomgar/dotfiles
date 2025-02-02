204a838fd167db45ce7fe96c006c3a8f37de7023
Patrick Haun <patrick@bomgar.email>
2025-02-02 22:23:24 +0100

    use neotest-golang

--- dot_config/nvim/lua/exact_plugins/neotest.lua
+++ dot_config/nvim/lua/exact_plugins/neotest.lua
@@ -4,12 +4,18 @@ return {
 		cmd = { "Neotest" },
 		ft = { "go", "rust", "javascriptreact", "javascript", "typescript", "typescriptreact" },
 		keys = {
-			{ "<leader>tr", function() require("neotest").run.run() end,                     desc = "Run test" },
-			{ "<leader>td", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "Debug test" },
-			{ "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end,   desc = "Run current file" },
-			{ "<leader>tp", "<cmd>Neotest output-panel<CR>",                                 desc = "Output panel" },
-			{ "<leader>to", "<cmd>Neotest output<CR>",                                       desc = "Output" },
-			{ "<leader>ts", "<cmd>Neotest summary<CR>",                                      desc = "summary" },
+			{ "<leader>tr", function() require("neotest").run.run() end,                                         desc = "[t]est [r]un nearest" },
+			{ "<leader>td", function() require("neotest").run.run({ suite = false, strategy = "dap" }) end,      desc = "[d]ebug nearest [t]est" },
+			{ "<leader>tD", function() require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" }) end, desc = "Debug current file" },
+			{ "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end,                       desc = "[t]est run [f]ile" },
+			{ "<leader>tp", function() require("neotest").output_panel.toggle() end,                             desc = "[t]est output [p]anel" },
+			{ "<leader>ts", function() require("neotest").summary.toggle() end,                                  desc = "[t]est [s]ummary" },
+			{ "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end,  desc = "[t]est [o]utput" },
+			{ "<leader>ta", function() require("neotest").run.attach() end,                                      desc = "[t]est [a]ttach" },
+			{ "<leader>tA", function() require("neotest").run.run(vim.uv.cwd()) end,                             desc = "[t]est [A]ll files" },
+			{ "<leader>tS", function() require("neotest").run.run({ suite = true }) end,                         desc = "[t]est [S]uite" },
+			{ "<leader>tl", function() require("neotest").run.run_last() end,                                    desc = "[t]est [l]ast" },
+			{ "<leader>tt", function() require("neotest").run.stop() end,                                        desc = "[t]est [t]erminate" },
 		},
 		dependencies = {
 			"nvim-lua/plenary.nvim",
@@ -17,13 +23,13 @@ return {
 			"haydenmeade/neotest-jest",
 			"rouge8/neotest-rust",
 			"nvim-neotest/nvim-nio",
-			"nvim-neotest/neotest-go",
+			{ "fredrikaverpil/neotest-golang", version = "*" }
 		},
 		config = function()
 			require("neotest").setup({
 				adapters = {
 					require("neotest-rust"),
-					require("neotest-go"),
+					require("neotest-golang")({}),
 					require("neotest-jest")({
 						jestCommand = "npm test --",
 						env = {},
