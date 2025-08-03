ad5bbb011093c5dc36193b82ef622cf0484f4a2c
Patrick Haun <patrick@bomgar.email>
2025-08-03 13:54:38 +0200

    add dadbod

--- dot_config/nvim/lua/exact_plugins/blink.lua
+++ dot_config/nvim/lua/exact_plugins/blink.lua
@@ -46,6 +46,12 @@ return {
 
 			sources = {
 				default = { 'lsp', 'path', 'snippets', 'buffer' },
+				per_filetype = {
+					sql = { 'snippets', 'dadbod', 'buffer' },
+				},
+				providers = {
+					dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+				},
 			},
 
 		},
---
+++ dot_config/nvim/lua/exact_plugins/dadbod.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		'kristijanhusak/vim-dadbod-ui',
+		dependencies = {
+			{ 'tpope/vim-dadbod',                     lazy = true },
+			{ 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+		},
+		cmd = {
+			'DBUI',
+			'DBUIToggle',
+			'DBUIAddConnection',
+			'DBUIFindBuffer',
+		},
+		init = function()
+			vim.g.db_ui_use_nerd_fonts = 1
+			vim.g.db_ui_execute_on_save = 0
+		end,
+	},
+}
