f2290fbe1452fd59cbe013cfcb8dedbff0654373
Patrick Haun <patrick@bomgar.email>
2025-08-29 16:13:26 +0200

    preview hunk

--- dot_config/nvim/lua/exact_plugins/gitsigns.lua
+++ dot_config/nvim/lua/exact_plugins/gitsigns.lua
@@ -4,8 +4,9 @@ return {
 		opts = {},
 		lazy = false,
 		keys = {
-			{ "<leader>gb", function() require("gitsigns").blame() end,      desc = "Git blame" },
-			{ "<leader>gB", function() require("gitsigns").blame_line() end, desc = "Git blame line" },
+			{ "<leader>gb", function() require("gitsigns").blame() end,               desc = "Git blame" },
+			{ "<leader>gp", function() require("gitsigns").preview_hunk_inline() end, desc = "Git blame" },
+			{ "<leader>gB", function() require("gitsigns").blame_line() end,          desc = "Git blame line" },
 		}
 	}
 }
