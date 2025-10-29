d45c895d643b687bee4b513a7b214627386b8d4b
Patrick Haun <patrick@bomgar.email>
2025-10-29 20:16:57 +0100

    niri focus color

---
+++ dot_local/share/dark-mode.d/executable_niri.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sed -i 's/active-color "#c4a7e7"/active-color "#907aa9"/' ~/.config/niri/config.kdl
+
---
+++ dot_local/share/light-mode.d/executable_niri.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sed -i 's/active-color "#907aa9"/active-color "#c4a7e7"/' ~/.config/niri/config.kdl
+
