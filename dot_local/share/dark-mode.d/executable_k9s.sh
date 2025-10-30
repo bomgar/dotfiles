2c437df48b0291499f1bba454e7c90b43bf76412
Patrick Haun <patrick@bomgar.email>
2025-10-30 12:18:06 +0100

    k9s theme switch

---
+++ dot_local/share/dark-mode.d/executable_k9s.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sed -i 's/skin: rose-pine-dawn/skin: rose-pine/' ~/.config/k9s/config.yaml
+
---
+++ dot_local/share/light-mode.d/executable_k9s.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sed -i 's/skin: rose-pine/skin: rose-pine-dawn/' ~/.config/k9s/config.yaml
+
