dd36be90c477aa509ac43e9713cfca59b2f36b2b
Patrick Haun <patrick@bomgar.email>
2025-11-25 08:08:05 +0100

    diffview nvim dead

--- dot_config/nvim/lua/exact_plugins/gitsigns.lua
+++ dot_config/nvim/lua/exact_plugins/gitsigns.lua
@@ -8,6 +8,7 @@ return {
 			{ "<leader>gp", function() require("gitsigns").preview_hunk_inline() end, desc = "Git preview hunk inline" },
 			{ "<leader>gP", function() require("gitsigns").preview_hunk() end,        desc = "Git preview hunk" },
 			{ "<leader>gB", function() require("gitsigns").blame_line() end,          desc = "Git blame line" },
+			{ "<leader>gW", function() require("gitsigns").toggle_word_diff() end,    desc = "Git toggle word diff" },
 		}
 	}
 }
