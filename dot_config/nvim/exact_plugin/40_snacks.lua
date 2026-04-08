0760dc2d10c73fa2c22b9a0106c7b942a202a645
Patrick Haun <patrick@bomgar.email>
2026-04-08 14:47:06 +0200

    show plugins

--- dot_config/nvim/exact_plugin/40_snacks.lua
+++ dot_config/nvim/exact_plugin/40_snacks.lua
@@ -10,6 +10,7 @@ require("snacks").setup({
 		preset = {
 			keys = {
 				{ icon = "📦", key = "U", desc = "Update plugins", action = ":lua vim.pack.update()" },
+				{ icon = "📦", key = "P", desc = "Show installed plugins", action = ":lua vim.pack.update(nil, { offline = true })" },
 				{ icon = "", key = "n", desc = "New File", action = ":ene | startinsert" },
 				{ icon = "", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
 				{ icon = "", key = "q", desc = "Quit", action = ":qa" },
