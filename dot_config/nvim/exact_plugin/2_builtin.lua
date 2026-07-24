2193e5de1387f0bff29e7c837bf8b903eb371dff
Patrick Haun <patrick@bomgar.email>
2026-07-24 21:18:41 +0200

    add tab switch keymaps

--- dot_config/nvim/exact_plugin/2_builtin.lua
+++ dot_config/nvim/exact_plugin/2_builtin.lua
@@ -1,2 +1,7 @@
 vim.cmd.packadd("nvim.difftool")
 vim.cmd.packadd("nvim.undotree")
+
+require("which-key").add({
+  { "[t", "<cmd>tabprevious<CR>", desc = "previous tab" },
+  { "]t", "<cmd>tabnext<CR>",     desc = "next tab" },
+})
